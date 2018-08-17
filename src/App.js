b320b5673b45e354b85091bf152fafdfe76386e5
Dmitry Kharitonov <d.kharitonov@gmail.com>
2018-08-17 20:49:05 +0200

    React router added

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { Link } from 'react-router-dom';
 import './App.css';
 
 class App extends Component {
@@ -7,12 +7,15 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">DAM Control Center</h1>
+
         </header>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload. Let's go.
+          Welcome to DAM Control Center
         </p>
+        <div>
+          <Link to="/">Login</Link>
+        </div>
       </div>
     );
   }
