248c476ddec87b676a0317f9b0c7ae48b1018538
Dmitry Kharitonov <d.kharitonov@gmail.com>
2018-08-17 23:43:08 +0200

    Setup Router to Home component

--- src/App.js
+++ src/App.js
@@ -1,22 +1,22 @@
 import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
+import Routes from './Routes';
+
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <h1 className="App-title">DAM Control Center</h1>
+      <Router>
+        <div className="App">
+          <header className="App-header">
+            <h1 className="App-title">DAM Control Center</h1>
 
-        </header>
-        <p className="App-intro">
-          Welcome to DAM Control Center
-        </p>
-        <div>
-          <Link to="/">Login</Link>
+          </header>
+
+          <Routes />
         </div>
-      </div>
+      </Router>
     );
   }
 }
