c3cdad568540a6cdc02332dd1f931486b6c5837b
Dmitry Kharitonov <d.kharitonov@gmail.com>
2018-08-21 16:05:03 +0200

    Added Authenticated and Unauthenticated Routes

--- src/Routes.js
+++ src/Routes.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import { Switch } from 'react-router-dom';
-import AppliedRoute from './components/AppliedRoute';
+import AuthenticatedRoute from "./components/AuthenticatedRoute";
+import UnauthenticatedRoute from "./components/UnauthenticatedRoute";
 import Home from './containers/Home';
 import Login from './containers/Login';
 
 export default ({ childProps }) =>
   <Switch>
-    <AppliedRoute path="/" exact component={Home} props={childProps}/>
-    <AppliedRoute path="/login" exact component={Login} props={childProps}/>
+    <AuthenticatedRoute path="/" exact component={Home} props={childProps}/>
+    <UnauthenticatedRoute path="/login" exact component={Login} props={childProps}/>
   </Switch>;
