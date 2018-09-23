474b7f429434ba3f255e19770a2167033a7a856d
Dmitry Kharitonov <d.kharitonov@gmail.com>
2018-09-23 17:28:09 +0300

    Change Router to HashRouter

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Router, Link } from 'react-router-dom';
+import { HashRouter as Router, Link } from 'react-router-dom';
 import Routes from './Routes';
 import { Auth } from 'aws-amplify';
 
